5b65e7f740b1f9cccba3842b0b9c32151b93165f
NithyaBalasubramaniam <127744007+NithyaBalasubramaniam@users.noreply.github.com>
2024-04-30 23:42:33 +0530

    Update FindTheMissingValue.java

--- day3/FindTheMissingValue.java
+++ day3/FindTheMissingValue.java
@@ -6,14 +6,18 @@ public class FindTheMissingValue {
 
 	public static void main(String[] args) {
 
-		int a[]={1, 4,3,2,8, 6, 7};
+		int a[]={1,4,3,2,8, 6, 7};
 		Arrays.sort(a);
-		for(int i=0;i<a.length;i++)
+		for(int i=0;i<a.length-1;i++)
 		{
-		  if(a[i]==i)
+		  if(a[i+1]!=a[i]+1)
 		  {
-			  System.out.println(a[i]);
+			  
+			  System.out.println(a[i]+1);
+			  
+
 		  }
+		 
 		}
 	}
 
