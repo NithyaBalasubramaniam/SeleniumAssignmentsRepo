944280909177cc2acd1ba27cc3728e83b1b3c641
NithyaBalasubramaniam <127744007+NithyaBalasubramaniam@users.noreply.github.com>
2024-05-21 23:52:19 +0530

    Update Ajio.java

--- week3/day1/HomeAssignments/List/Ajio.java
+++ week3/day1/HomeAssignments/List/Ajio.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Ajio {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -20,11 +20,33 @@ public class Ajio {
 		//search box, type as "bags" and press enter & select men and fashion bags
 		driver.findElement(By.xpath("//input[contains(@class,'react-autosuggest__input react-autosuggest')]")).sendKeys("bags",Keys.ENTER);
 		driver.findElement(By.xpath("//input[@id='Men']/following-sibling::label")).click();
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//label[contains(@class,'Men - Fashion Bags')]")).click();
+		Thread.sleep(3000);
 		
 	    //print the count of bags
 		String text = driver.findElement(By.xpath("//div[@class='length']")).getText();
-		System.out.println(text);
+		System.out.println("The count of bags found "+text);
+		
+		//Getting the list of brands displayed in page
+		List<WebElement> brands = driver.findElements(By.xpath("//div[@class='brand']"));
+		System.out.println(brands.size() +" of brands found in the page");
+		System.out.print("[" );
+		for(WebElement i : brands)
+		{
+		System.out.print(i.getText()+",");
+		}
+		System.out.println("]");
+		
+		//Getting list of Bag names displayed in page
+		List<WebElement> bag_Names = driver.findElements(By.xpath("//div[@class='nameCls']"));
+		System.out.println(bag_Names.size() +" of bag names found in the page");
+		System.out.print("[" );
+		for (WebElement j : bag_Names)
+		{
+			System.out.print(j.getText()+",");
+		}
+		System.out.println("]");
 	}
 
 }
